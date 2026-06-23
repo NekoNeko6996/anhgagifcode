0d1eb6e0001168b51c7f25c4c7a87321c27e81ab
NekoNeko6996 <lqm231231@gmail.com>
2026-06-23 21:48:24 +0700

    first commit

---
+++ src/main/java/com/project/anhgagifcode/AnhgagifcodeApplication.java
@@ -0,0 +1,13 @@
+package com.project.anhgagifcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnhgagifcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnhgagifcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/anhgagifcode/AnhgagifcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.anhgagifcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnhgagifcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
